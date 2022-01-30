80819c7efc1c197f600d11d1da2eccf8c1f91392
Alejandro Sánchez <54010460+Alejandro10m10@users.noreply.github.com>
2022-01-30 15:08:53 -0600

    Update the function isNumberInBox
    The function is able to find if a determinate number belongs to a determinate box in sudoku’s board.

--- src/js/main.js
+++ src/js/main.js
@@ -12,42 +12,39 @@ let board = [
 	[0,0,7,0,4,0,2,0,3],
 ];
 
-let board2 = [
-   [7,0,2],
-   [0,0,0],
-   [1,0,0]
-];
 
 function isNumberInRow(board, row, number){
-    for(let i = 0; i < BOARD_SIZE ; i++){
-        if( board[row][i] === number){
-            return true;
-        }
-    }
-    return false;
+	for(let i = 0; i < BOARD_SIZE ; i++){
+		if( board[row][i] === number){
+			return true;
+		}
+	}
+	return false;
 }
 
 function isNumberInColumn(board, column, number){
-    for(let i = 0; i < BOARD_SIZE ; i++){
-        if( board[i][column] === number){
-            return true;
-        }
-    }
-    return false;
+	for(let i = 0; i < BOARD_SIZE ; i++){
+		if( board[i][column] === number){
+			return true;
+		}
+	}
+	return false;
 }
 
 function isNumberInBox(board, row, column, number){
-    if(row < 3 && column < 3){
-       for(let i = 0; i < 3 ; i++){
-        	if( isNumberInRow(board, i, number) ){
-            	return true;
-        	}
-    	}
-    	return false;
-    }
+	let boxRow = row - row % 3;
+	let boxColumn = column - column % 3;
+	
+	for(let i = boxRow ; i < boxRow + 3 ; i++){
+	    for(let j = boxColumn ; j < boxColumn + 3 ; j++){
+	    	if(board[i][j] === number){
+	    	    return true;
+	    	}
+	    }
+	}
+	return false;
 }
 
 console.log(isNumberInRow(board, 0, 3));
 console.log(isNumberInColumn(board, 1, 3));
-console.log(isNumberInBox(board2, 1, 0, 8));
-
+console.log(isNumberInBox(board, 8, 8, 7));
