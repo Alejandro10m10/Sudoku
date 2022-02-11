7b9ecbd5ab8417551b461efb4d162d31d390cd0a
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-02-10 23:54:41 -0600

    Rotate arrow on select dropdown

--- src/js/view-dropdown.js
+++ src/js/view-dropdown.js
@@ -1,17 +1,23 @@
 let btnGameMode = document.querySelector('#mode-game-dropdown'),
     btnLanguage = document.querySelector('#language-dropdown'),
     menuDropdownElement = document.querySelector('.mode-menu-dropdown'),
-    languageDropdownElement = document.querySelector('.language-menu-dropdown');
+    languageDropdownElement = document.querySelector('.language-menu-dropdown'),
+    arrowGameMode = document.querySelector('.mode-menu__arrowIcon'),
+    arrowLanguage = document.querySelector('.language-menu__arrowIcon');
 
-const classNameNoDisplay = 'no-display';
+const classNameNoDisplay = 'no-display',
+      classRotateElement = 'rotate';
 
-btnLanguage.addEventListener('click', () => showDropdownList(languageDropdownElement));
-btnGameMode.addEventListener('click', () => showDropdownList(menuDropdownElement));
+btnLanguage.addEventListener('click', () => showDropdownList(languageDropdownElement, arrowLanguage));
+btnGameMode.addEventListener('click', () => showDropdownList(menuDropdownElement, arrowGameMode));
 
 function showDropdownList(element, arrow){
     if(element.classList.contains(classNameNoDisplay)){
         element.classList.remove(classNameNoDisplay);
+        arrow.classList.add(classRotateElement);
     } else{
         element.classList.add(classNameNoDisplay);
+        arrow.classList.remove(classRotateElement);
     }
+
 }
