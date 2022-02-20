7a5e660491ce4b1dce108bf40867559988ad3596
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-02-19 23:38:35 -0600

    Adding the function to show the classic and killer dropDown element

--- src/js/view-dropdown.js
+++ src/js/view-dropdown.js
@@ -3,13 +3,21 @@ let btnGameMode = document.querySelector('#mode-game-dropdown'),
     menuDropdownElement = document.querySelector('.mode-menu-dropdown'),
     languageDropdownElement = document.querySelector('.language-menu-dropdown'),
     arrowGameMode = document.querySelector('.mode-menu__arrowIcon'),
-    arrowLanguage = document.querySelector('.language-menu__arrowIcon');
+    arrowLanguage = document.querySelector('.language-menu__arrowIcon'),
+    btnClassicMode = document.querySelector('#btnClassicMode'),
+    btnKillerMode = document.querySelector('#btnKillerMode'),
+    classicModeDropdownElement = document.querySelector('.new-game-menu__classicOptions'),
+    killerModeDropdownElement = document.querySelector('.new-game-menu__killerOptions');
+    
+
 
 const classNameNoDisplay = 'no-display',
       classRotateElement = 'rotate';
 
 btnLanguage.addEventListener('click', () => showDropdownList(languageDropdownElement, arrowLanguage, menuDropdownElement, arrowGameMode));
 btnGameMode.addEventListener('click', () => showDropdownList(menuDropdownElement, arrowGameMode, languageDropdownElement, arrowLanguage));
+btnClassicMode.addEventListener('click', selectMode); //Value true -> Classic
+btnKillerMode.addEventListener('click', selectMode); //Value false -> Killer
 
 function showDropdownList(firstElement, firstArrow, secondElement, secondArrow){
     if(firstElement.classList.contains(classNameNoDisplay)){
@@ -23,3 +31,22 @@ function showDropdownList(firstElement, firstArrow, secondElement, secondArrow){
     }
 }
 
+
+function selectMode(){
+    if(killerModeDropdownElement.classList.contains('no-display')){
+        killerModeDropdownElement.classList.remove('no-display');
+        btnKillerMode.classList.add('new-game-menu__buttons--selected');
+    } else{
+        killerModeDropdownElement.classList.add('no-display');
+        btnKillerMode.classList.remove('new-game-menu__buttons--selected');
+    }
+
+    if(classicModeDropdownElement.classList.contains('no-display')){
+        classicModeDropdownElement.classList.remove('no-display');
+        btnClassicMode.classList.add('new-game-menu__buttons--selected');
+    } else{
+        classicModeDropdownElement.classList.add('no-display');
+        btnClassicMode.classList.remove('new-game-menu__buttons--selected');
+    }
+
+}
