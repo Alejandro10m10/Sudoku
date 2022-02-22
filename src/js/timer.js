cff3c7b49728f6cc2a21025814b3d235e6f21307
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-02-22 10:56:48 -0600

    Adding the function to show and hide the pause overlay element with the play and pause buttons

--- src/js/timer.js
+++ src/js/timer.js
@@ -3,6 +3,9 @@ let timerElement = document.querySelector('#gameTime'),
     playGame = document.querySelector('#btnPlayGame'),
     newGame = document.querySelector('#btnNewGame');
 
+let overlayElement = createOverlayElement(),
+    gameContent = document.querySelector('.game__content');;
+
 pauseGame.addEventListener('click', pauseTimer);
 playGame.addEventListener('click', playTimer);
 newGameEventListener(newGame);
@@ -59,6 +62,7 @@ function pauseTimer(){
     clearTimeout(t);
     displayElement(pauseGame, false);
     displayElement(playGame, true);
+    showPauseOverlay(true);
 }
 
 function resetTimer(){
@@ -70,6 +74,7 @@ function playTimer(){
     timer();
     displayElement(pauseGame, true);
     displayElement(playGame, false);
+    showPauseOverlay(false);
 }
 
 function displayElement(element, value){
@@ -77,3 +82,19 @@ function displayElement(element, value){
         ? element.classList.remove('no-display') 
         : element.classList.add('no-display');
 }
+
+function createOverlayElement(){
+    let overlayElement = document.createElement("div");
+    overlayElement.className = "game__content__pause-overlay";
+    let pauseIcon = '<svg xmlns="http://www.w3.org/2000/svg" class="icon-play-big game__content__pause-overlay--icon" viewBox="0 0 60 60"><g fill="none" fill-rule="evenodd"><circle cx="30" cy="30" r="30" fill="#0072E3"></circle><path fill="#FFF" d="M39.12 31.98l-12.56 8.64a2.4 2.4 0 01-3.76-1.98V21.36a2.4 2.4 0 013.76-1.97l12.56 8.63a2.4 2.4 0 010 3.96z"></path></g></svg>'
+    overlayElement.innerHTML = pauseIcon;
+
+    return overlayElement;
+}
+
+function showPauseOverlay(value){
+    (value)
+        ?  gameContent.appendChild(overlayElement)
+        : gameContent.removeChild(overlayElement);
+}
+
